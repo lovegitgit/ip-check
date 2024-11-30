27792640ec8eec5bf92548842915d4aa6d75db30
IronXiao <xiaoleigs@gmail.com>
2024-11-30 20:19:48 +0800

    bump version to V2.0.3

--- README.md
+++ README.md
@@ -228,6 +228,6 @@ optional arguments:
 [GNU General Public License, version 3](https://www.gnu.org/licenses/gpl-3.0.html)
 
 ## Release Note
-- V2.0.2
-  - 修复一些bug
+- V2.0.3
+  - 代码重构
 
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name='ip-check',
-    version='2.0.2',
+    version='2.0.3',
     description='Powerful cdn network speed test utils.',
     long_description=long_description,
     long_description_content_type='text/markdown',
